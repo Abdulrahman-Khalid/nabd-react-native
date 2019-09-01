e1d74a3327d711fc63990390fb47145038a6561b
Nada Ashraf <nadaashraf11@icloud.com>
2019-09-01 02:12:20 +0200

    Added StepIndicator

--- src/Router.js
+++ src/Router.js
@@ -1,71 +1,71 @@
-import React, { Component } from 'react';
-import { Scene, Router } from 'react-native-router-flux';
-import WhoAmI from './app/welcome/WhoAmI';
-import IamDoctor from './app/welcome/IamDoctor';
-import IamAmbulance from './app/welcome/IamAmbulance';
-import IamUser from './app/welcome/IamUser';
-import IamParamedic from './app/welcome/IamParamedic';
-import Register from './app/welcome/Register';
-import VerifySignup from './app/welcome/PhoneVerification/Animated';
-import SignIn from './app/welcome/SignIn';
-import UserHome from './app/home/UserHome';
-import { argonTheme } from './constants';
-import InjuriesList from './app/firstAid/InjuriesList';
-import FirstAidDetails from './app/firstAid/FirstAidDetails';
-class RouterComponent extends Component {
-  render() {
-    return (
-      <Router
-        // navigationBarStyle={{
-        //   bacIamDoctorkgroundColor: '#EF171D'
-        // }}
-        titleStyle={{
-          fontWeight: 'bold',
-          color: '#000'
-        }}
-        tintColor={argonTheme.COLORS.APP}
-      >
-        <Scene key="root" hideNavBar>
-          <Scene key="welcome" intial headerLayoutPreset="center">
-            <Scene key="whoRU" component={WhoAmI} title="Nabd" initial />
-            <Scene key="iUser" component={IamUser} title="User" />
-            <Scene key="iDoctor" component={IamDoctor} title="Doctor" />
-            <Scene
-              key="iParamedic"
-              component={IamParamedic}
-              title="Paramedic"
-            />
-            <Scene
-              key="iAmbulance"
-              component={IamAmbulance}
-              title="Ambulance"
-            />
-            <Scene key="signup" component={Register} title="Sign up" />
-            <Scene key="signin" component={SignIn} title="Sign in" />
-            <Scene key="verifySignup" component={VerifySignup} title="Verify" />
-          </Scene>
+// import React, { Component } from 'react';
+// import { Scene, Router } from 'react-native-router-flux';
+// import WhoAmI from './app/welcome/WhoAmI';
+// import IamDoctor from './app/welcome/IamDoctor';
+// import IamAmbulance from './app/welcome/IamAmbulance';
+// import IamUser from './app/welcome/IamUser';
+// import IamParamedic from './app/welcome/IamParamedic';
+// import Register from './app/welcome/Register';
+// import VerifySignup from './app/welcome/PhoneVerification/Animated';
+// import SignIn from './app/welcome/SignIn';
+// import UserHome from './app/home/UserHome';
+// import { argonTheme } from './constants';
+// import InjuriesList from './app/firstAid/InjuriesList';
+// import FirstAidDetails from './app/firstAid/FirstAidDetails';
+// class RouterComponent extends Component {
+//   render() {
+//     return (
+//       <Router
+//         // navigationBarStyle={{
+//         //   bacIamDoctorkgroundColor: '#EF171D'
+//         // }}
+//         titleStyle={{
+//           fontWeight: 'bold',
+//           color: '#000'
+//         }}
+//         tintColor={argonTheme.COLORS.APP}
+//       >
+//         <Scene key="root" hideNavBar>
+//           <Scene key="welcome" intial headerLayoutPreset="center">
+//             <Scene key="whoRU" component={WhoAmI} title="Nabd" initial />
+//             <Scene key="iUser" component={IamUser} title="User" />
+//             <Scene key="iDoctor" component={IamDoctor} title="Doctor" />
+//             <Scene
+//               key="iParamedic"
+//               component={IamParamedic}
+//               title="Paramedic"
+//             />
+//             <Scene
+//               key="iAmbulance"
+//               component={IamAmbulance}
+//               title="Ambulance"
+//             />
+//             <Scene key="signup" component={Register} title="Sign up" />
+//             <Scene key="signin" component={SignIn} title="Sign in" />
+//             <Scene key="verifySignup" component={VerifySignup} title="Verify" />
+//           </Scene>
 
-          <Scene key="home">
-            <Scene key="userHome" component={UserHome} title="Home" />
-          </Scene>
+//           <Scene key="home">
+//             <Scene key="userHome" component={UserHome} title="Home" />
+//           </Scene>
 
-          <Scene key="FirstAid">
-            <Scene
-              key="InjuriesList"
-              component={InjuriesList}
-              // title="First Aid Instructions"
-              initial
-            />
-            <Scene
-              // type="reset"
-              key="FirstAidDetails"
-              component={FirstAidDetails}
-            />
-          </Scene>
-        </Scene>
-      </Router>
-    );
-  }
-}
+//           <Scene key="FirstAid">
+//             <Scene
+//               key="InjuriesList"
+//               component={InjuriesList}
+//               // title="First Aid Instructions"
+//               initial
+//             />
+//             <Scene
+//               // type="reset"
+//               key="FirstAidDetails"
+//               component={FirstAidDetails}
+//             />
+//           </Scene>
+//         </Scene>
+//       </Router>
+//     );
+//   }
+// }
 
-export default RouterComponent;
+// export default RouterComponent;
