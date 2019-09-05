6cac4f9d04595c716ffb6e40306d7ba975db3a82
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2019-09-05 09:55:08 +0200

    Configure Redux Persist

--- src/config/ReduxPersist.js
+++ src/config/ReduxPersist.js
@@ -1,4 +1,3 @@
-import immutablePersistenceTransform from '../Services/ImmutablePersistenceTransform'
 import { AsyncStorage } from 'react-native'
 
 // More info here:  https://shift.infinite.red/shipping-persistant-reducers-7341691232b1
@@ -9,11 +8,11 @@ const REDUX_PERSIST = {
     key: 'primary',
     storage: AsyncStorage,
     // Reducer keys that you do NOT want stored to persistence here.
-    blacklist: ['login', 'search', 'nav'],
+    // blacklist: [],
+    
     // Optionally, just specify the keys you DO want stored to persistence.
     // An empty array means 'don't store any reducers' -> infinitered/ignite#409
-    // whitelist: [],
-    transforms: [immutablePersistenceTransform]
+    whitelist: [],
   }
 }
 
