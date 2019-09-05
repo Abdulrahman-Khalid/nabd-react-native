a314531e2d8eef6b4ab1a16fab82232927134532
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2019-09-05 07:15:30 +0200

    Change Splash Screen Status Bar Color

--- android/app/src/main/java/com/nabd/MainActivity.java
+++ android/app/src/main/java/com/nabd/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        SplashScreen.show(this);
+        SplashScreen.show(this, R.style.SplashScreenTheme);
         return "NABD";
     }
 }
