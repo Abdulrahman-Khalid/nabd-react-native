180b37a12e8d829226d895de1827dfc11561f4d4
Nader <nader_abdelghani@hotmail.com>
2019-10-17 16:58:47 +0200

    Add Location Picker Modal to 'Add Incident' Form

--- src/app/home/AddIncident.js
+++ src/app/home/AddIncident.js
@@ -7,6 +7,8 @@ import {
   Button,
   StyleSheet,
   ToastAndroid,
+  Modal,
+  KeyboardAvoidingView
 } from 'react-native';
 import axios from 'axios';
 import ImagePicker from 'react-native-image-picker';
@@ -14,6 +16,7 @@ import { Actions } from 'react-native-router-flux';
 import { TextInput } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/dist/MaterialCommunityIcons';
 import ActionSheet from 'react-native-action-sheet';
+import { LocationPicker } from '../../components';
 
 const actionSheetButtons = [
   'Take a picture',
@@ -24,9 +27,9 @@ export default class AddIncident extends Component {
   state = {
     text: '',
     numberToCall: null,
-    count: 280,
     photo: null,
     media: null,
+    modalVisible: false
   };
 
   /**
@@ -144,195 +147,137 @@ export default class AddIncident extends Component {
     ToastAndroid.show('Loading', ToastAndroid.SHORT);
   }
 
+  modalCancelOnPress = () => {
+    this.setState({
+      modalVisible: false
+    });
+  }
+
   render() {
-    const buttonDisabled = this.state.count <= 0 || this.state.count >= 279;
     const maxLength = 240;
     return (
-      // <View style={{ flex: 1 }}>
-      //   <View style={{ flex: 10, flexDirection: 'row' }}>
-      //     <View style={{ flex: 1 }}>
-      //       <Image
-      //         source={{ uri: this.state.img }}
-      //         style={{
-      //           width: 40,
-      //           height: 40,
-      //           borderRadius: 20,
-      //           alignSelf: 'flex-start',
-      //           marginTop: '15%',
-      //           marginLeft: '15%'
-      //         }}
-      //       />
-      //     </View>
-      //     <View style={{ flex: 6 }}>
-      //       <TextInput
-      //         onChangeText={t => {
-      //           this.setState({ text: t, count: maxLength - t.length });
-      //         }}
-      //         placeholder="What's happening?"
-      //         placeholderTextColor="#657786"
-      //         style={{ fontSize: 18 }}
-      //         multiline
-      //       >
-      //       </TextInput>
-      //       {this.state.photo === null ? null : (
-      //         <View
-      //           style={{
-      //             height: 200,
-      //             width: '80%',
-      //             marginTop: '2%',
-      //             borderWidth: 1,
-      //             borderColor: '#AAB8C2',
-      //             borderRadius: 5
-      //           }}
-      //         >
-      //           <Image
-      //             source={this.state.photo}
-      //             style={{ height: '100%', width: '100%' }}
-      //           />
-      //         </View>
-      //       )}
-      //     </View>
-      //   </View>
-      //   <View
-      //     style={{
-      //       flex: 1,
-      //       flexDirection: 'row',
-      //       borderTopWidth: 0.75,
-      //       borderTopColor: '#AAB8C2',
-      //       backgroundColor: '#FFFFFF'
-      //     }}
-      //   >
-      //     <Feather
-      //       name="camera"
-      //       size={36}
-      //       color="rgb(29, 161, 242)"
-      //       onPress={this.handleCam}
-      //       style={{ marginLeft: '3%', marginTop: '1%' }}
-      //     />
-      //     <FontAwesome
-      //       name="photo"
-      //       size={36}
-      //       color="rgb(29, 161, 242)"
-      //       onPress={this.handleChoosePhoto}
-      //       style={{ marginLeft: '3%', marginTop: '1%' }}
-      //     />
-      //     <Text style={{ marginLeft: '53%', marginTop: '4%' }}>
-      //       {this.state.count} / 280
-      //     </Text>
-      //   </View>
-      // </View>
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'stretch',
-          padding: 20
-        }}
-      >
-        <TouchableOpacity
-          style={
-            this.state.photo === null
-              ? styles.imageInputContainerEmpty
-              : styles.imageInputContainerFull
-          }
-          onPress={() => {
-            ActionSheet.showActionSheetWithOptions(
-              {
-                options: actionSheetButtons,
-                cancelButtonIndex: 3,
-                destructiveButtonIndex: 2,
-                tintColor: 'blue'
-              },
-              buttonIndex => {
-                console.log('button clicked :', buttonIndex);
-                switch (buttonIndex) {
-                  case 0:
-                    this.handleCam();
-                    break;
-                  case 1:
-                    this.handleChoosePhoto();
-                    break;
-                  default:
-                    break;
-                }
-              }
-            );
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" enabled>
+        <Modal visible={this.state.modalVisible}>
+          <LocationPicker cancelOnPress={this.modalCancelOnPress} />
+        </Modal>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'stretch',
+            padding: 20
           }}
         >
-          {this.state.photo === null ? (
-            <Icon name="image-plus" size={80} color="gray" />
-          ) : (
-            <View
-              style={{
-                height: '100%',
-                width: '100%',
-                borderRadius: 20
-              }}
-            >
-              <Image
-                source={this.state.photo}
+          <TouchableOpacity
+            style={
+              this.state.photo === null
+                ? styles.imageInputContainerEmpty
+                : styles.imageInputContainerFull
+            }
+            onPress={() => {
+              ActionSheet.showActionSheetWithOptions(
+                {
+                  options: actionSheetButtons,
+                  cancelButtonIndex: 3,
+                  destructiveButtonIndex: 2,
+                  tintColor: 'blue'
+                },
+                buttonIndex => {
+                  console.log('button clicked :', buttonIndex);
+                  switch (buttonIndex) {
+                    case 0:
+                      this.handleCam();
+                      break;
+                    case 1:
+                      this.handleChoosePhoto();
+                      break;
+                    default:
+                      break;
+                  }
+                }
+              );
+            }}
+          >
+            {this.state.photo === null ? (
+              <Icon name="image-plus" size={80} color="gray" />
+            ) : (
+              <View
                 style={{
                   height: '100%',
                   width: '100%',
                   borderRadius: 20
                 }}
-              />
-            </View>
-          )}
-        </TouchableOpacity>
-        <View style={{ flex: 0.5, marginBottom: 20, justifyContent: 'center' }}>
-          <TextInput
-            label="Number to Call (optional)"
-            mode="outlined"
-            value={this.state.numberToCall}
-            onChangeText={t => {
-              this.setState({ numberToCall: t });
-            }}
-            dataDetectorTypes="phoneNumber"
-            style={{ fontSize: 18 }}
-            theme={{ colors: { background: '#EAE9EF' } }}
-            keyboardType="phone-pad"
-            textContentType="telephoneNumber"
-          />
-        </View>
-        <View style={{ flex: 2, marginBottom: 10 }}>
-          <TextInput
-            label="Description (required)"
-            mode="outlined"
-            value={this.state.text}
-            onChangeText={t => {
-              this.setState({ text: t, count: maxLength - t.length });
-            }}
-            placeholder="What's happening?"
-            style={{ fontSize: 18, height: '100%' }}
-            theme={{ colors: { background: '#EAE9EF' } }}
-            multiline
-            maxLength={maxLength}
-          />
-        </View>
-        <View style={styles.buttonsContainer}>
-          <TouchableOpacity
-            style={styles.buttonContainer}
-            onPress={() => {
-              console.log('submit pressed');
-            }}
-          >
-            <View
-              style={[
-                styles.button,
-                {
-                  backgroundColor: '#FFFF'
-                }
-              ]}
-            >
-              <Text style={{ color: '#b3b3b2', fontFamily: 'Manjari-Bold' }}>
-                Next
-              </Text>
-            </View>
+              >
+                <Image
+                  source={this.state.photo}
+                  style={{
+                    height: '100%',
+                    width: '100%',
+                    borderRadius: 20
+                  }}
+                />
+              </View>
+            )}
           </TouchableOpacity>
+          <View
+            style={{ flex: 0.5, marginBottom: 20, justifyContent: 'center' }}
+          >
+            <TextInput
+              label="Number to Call (optional)"
+              mode="outlined"
+              value={this.state.numberToCall}
+              onChangeText={t => {
+                this.setState({ numberToCall: t });
+              }}
+              dataDetectorTypes="phoneNumber"
+              style={{ fontSize: 18 }}
+              theme={{ colors: { background: '#EAE9EF' } }}
+              keyboardType="phone-pad"
+              textContentType="telephoneNumber"
+            />
+          </View>
+          <View style={{ flex: 2, marginBottom: 10 }}>
+            <TextInput
+              label="Description (required)"
+              mode="outlined"
+              value={this.state.text}
+              onChangeText={text => {
+                this.setState({ text });
+              }}
+              placeholder="What's happening?"
+              style={{ fontSize: 18, height: '100%' }}
+              theme={{ colors: { background: '#EAE9EF' } }}
+              multiline
+              maxLength={maxLength}
+            />
+          </View>
+          <View style={styles.buttonsContainer}>
+            <TouchableOpacity
+              style={styles.buttonContainer}
+              onPress={() => {
+                // Actions.LocationPicker();
+                this.setState({
+                  modalVisible: true
+                });
+              }}
+            >
+              <View
+                style={[
+                  styles.button,
+                  {
+                    backgroundColor: '#FFFF'
+                  }
+                ]}
+              >
+                <Text style={{ color: '#b3b3b2', fontFamily: 'Manjari-Bold' }}>
+                  Next
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
