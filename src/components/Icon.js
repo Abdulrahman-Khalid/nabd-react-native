36f8e9dc50fd12c9bcb492d25f3a732abdecc36a
Nader <nader_abdelghani@hotmail.com>
2019-10-22 23:11:25 +0200

    Add Animated Icon Component

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -3,11 +3,14 @@ import icoMoonConfigArgon from '../config/ArgonFontConfig.json';
 import icoMoonConfigLinear from '../config/LinearFontConfig.json';
 import icoMoonConfigFlat from '../config/FlatIconConfig.json';
 import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import { Animated } from 'react-native';
 import { Icon } from 'galio-framework';
+import * as Animatable from 'react-native-animatable';
 
 const IconArgonExtra = createIconSetFromIcoMoon(icoMoonConfigArgon);
 const IconLinearExtra = createIconSetFromIcoMoon(icoMoonConfigLinear);
 const IconFlatExtra = createIconSetFromIcoMoon(icoMoonConfigFlat);
+const AnimatedFlatIcon = Animatable.createAnimatableComponent(IconFlatExtra);
 
 class IconExtra extends React.Component {
   render() {
@@ -22,6 +25,9 @@ class IconExtra extends React.Component {
           break;
         case 'flaticon':
           return <IconFlatExtra name={name} family={family} {...rest} />;
+          break;
+        case 'animatedFlaticon':
+          return <AnimatedFlatIcon name={name} family={family} {...rest} />;
           break;
         default:
           return <Icon name={name} family={family} {...rest} />;
