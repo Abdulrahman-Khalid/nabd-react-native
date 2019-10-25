531a99c5a677d0a6a20ce1b6f06ef0da2c30514a
Nader <nader_abdelghani@hotmail.com>
2019-10-25 14:37:57 +0200

    Customize Switch Styling

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,4 +1,3 @@
-
 /*
 
 how to use:
@@ -51,168 +50,204 @@ small example: ...
 
 import React, { Component } from 'react';
 import {
-    View,
-    Text,
-    TouchableOpacity,
-    StyleSheet,
-    Animated
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Animated
 } from 'react-native';
 import PropTypes from 'prop-types';
 
 export default class SwitchButton extends Component {
+  static propTypes = {
+    onValueChange: PropTypes.func
+  };
 
-    static propTypes = {
-		onValueChange: PropTypes.func
-	};
+  static defaultProps = {
+    onValueChange: () => null
+  };
 
-	static defaultProps = {
-		onValueChange: () => null
-	};
+  constructor() {
+    super();
 
-    constructor() {
-        super();
+    this.state = {
+      activeSwitch: 1,
+      sbWidth: 180,
+      sbHeight: 50,
+      direction: 'ltr',
+      offsetX: new Animated.Value(0)
+    };
 
-        this.state = {
-          activeSwitch: 1,
-          sbWidth: 100,
-          sbHeight: 44,
-          direction: 'ltr',
-          offsetX: new Animated.Value(0)
-        };
+    this._switchDirection = this._switchDirection.bind(this);
+  }
 
-        this._switchDirection = this._switchDirection.bind(this);
+  _switchDirection(direction) {
+    let dir = 'row';
+
+    // if (direction === 'rtl') {
+    //   dir = 'row-reverse';
+    // } else {
+    //   dir = 'row';
+    // }
+    return dir;
+  }
+
+  _switchThump(direction) {
+    const { onValueChange, disabled } = this.props;
+    let dirsign = 1;
+    if (direction === 'rtl') {
+      dirsign = -1;
+    } else {
+      dirsign = 1;
     }
 
-    _switchDirection(direction) {
-        let dir = 'row';
+    if (this.state.activeSwitch === 1) {
+      this.setState({ activeSwitch: 2 }, () =>
+        onValueChange(this.state.activeSwitch)
+      );
 
-        if (direction === 'rtl') {
-            dir = 'row-reverse';
-        }
-        else {
-            dir = 'row';
-        }
-        return dir;
+      Animated.timing(this.state.offsetX, {
+        toValue:
+          ((this.props.switchWidth || this.state.sbWidth) / 2) * dirsign,
+        duration: this.props.switchSpeedChange || 100
+      }).start();
+    } else {
+      this.setState({ activeSwitch: 1 }, () =>
+        onValueChange(this.state.activeSwitch)
+      );
+      Animated.timing(this.state.offsetX, {
+        toValue: 0,
+        duration: this.props.switchSpeedChange || 100
+      }).start();
     }
+  }
 
-    _switchThump(direction) {
-        const { onValueChange, disabled } = this.props;
-        let dirsign = 1;
-        if (direction === 'rtl') {
-            dirsign = -1;
-        }
-        else {
-            dirsign = 1;
-        }
-
-        if (this.state.activeSwitch === 1){
-            this.setState({ activeSwitch: 2 }, () => onValueChange(this.state.activeSwitch));
-
-            Animated.timing(
-                this.state.offsetX,
+  render() {
+    return (
+      <View>
+        <TouchableOpacity
+          activeOpacity={1}
+          onPress={() => {
+            this._switchThump(
+              this.props.switchdirection || this.state.direction
+            );
+          }}
+        >
+          <View
+            style={[
+              {
+                width: this.props.switchWidth || this.state.sbWidth,
+                height: this.props.switchHeight || this.state.sbHeight,
+                borderRadius:
+                  this.props.switchBorderRadius !== undefined
+                    ? this.props.switchBorderRadius
+                    : this.state.sbHeight / 2,
+                backgroundColor: this.props.switchBackgroundColor || '#cac6d1'
+              }
+            ]}
+          >
+            <View
+              style={[
+                {
+                  flexDirection: this._switchDirection(
+                    this.props.switchdirection || this.state.direction
+                  )
+                }
+              ]}
+            >
+              <Animated.View
+                style={{ transform: [{ translateX: this.state.offsetX }] }}
+              >
+                <View
+                  style={[
+                    switchStyles.wayBtnActive,
                     {
-                        toValue: (((this.props.switchWidth || this.state.sbWidth) / 2) - 6) * dirsign,
-                        duration: this.props.switchSpeedChange || 100
+                      width:
+                        this.props.switchWidth / 2 || this.state.sbWidth / 2,
+                      height: this.props.switchHeight || this.state.sbHeight,
+                      borderRadius:
+                        this.props.switchBorderRadius !== undefined
+                          ? this.props.switchBorderRadius
+                          : this.state.sbHeight / 2,
+                      borderColor: this.props.btnBorderColor || '#000',
+                      backgroundColor: this.props.btnBackgroundColor || '#ffff'
                     }
-            ).start();
-        }
-        else {
-            this.setState({ activeSwitch: 1 }, () => onValueChange(this.state.activeSwitch));
-            Animated.timing(
-                this.state.offsetX,
-                    {
-                        toValue: 0,
-                        duration: this.props.switchSpeedChange || 100
-                    }
-            ).start();
-        }
+                  ]}
+                />
+              </Animated.View>
 
-    }
+              <View
+                style={[
+                  switchStyles.textPos,
+                  {
+                    width: this.props.switchWidth / 2 || this.state.sbWidth / 2,
+                    height:
+                      this.props.switchHeight - 6 || this.state.sbHeight - 6,
+                    left: 0
+                  }
+                ]}
+              >
+                <Text
+                  style={[
+                    this.state.activeSwitch === 1
+                      ? { color: this.props.activeFontColor || '#000' }
+                      : { color: this.props.fontColor || '#817d84' }
+                  ], { fontWeight: 'bold' }}
+                >
+                  {this.props.text1 || 'ON'}
+                </Text>
+              </View>
 
-    render() {
-
-        return (
-
-                <View>
-                <TouchableOpacity activeOpacity={1} onPress={ () => { this._switchThump(this.props.switchdirection || this.state.direction) } }>
-                    <View
-                        style={[{
-                                width: this.props.switchWidth || this.state.sbWidth,
-                                height: this.props.switchHeight || this.state.sbHeight,
-                                borderRadius: this.props.switchBorderRadius !== undefined ? this.props.switchBorderRadius : this.state.sbHeight / 2,
-                                borderWidth: 1,
-                                borderColor: this.props.switchBorderColor || "#d4d4d4",
-                                backgroundColor: this.props.switchBackgroundColor || "#fff"
-                            }]}
-                    >
-                            <View style={[{ flexDirection: this._switchDirection(this.props.switchdirection || this.state.direction) }]} >
-
-                                <Animated.View style={{ transform: [{translateX: this.state.offsetX }] }}>
-                                    <View
-                                        style={[switchStyles.wayBtnActive,
-                                                {
-                                                    width: this.props.switchWidth / 2 || this.state.sbWidth / 2,
-                                                    height: this.props.switchHeight - 6 || this.state.sbHeight - 6,
-                                                    borderRadius: this.props.switchBorderRadius !== undefined ? this.props.switchBorderRadius : this.state.sbHeight / 2,
-                                                    borderColor: this.props.btnBorderColor || "#00a4b9",
-                                                    backgroundColor: this.props.btnBackgroundColor || "#00bcd4"
-                                                }]}
-                                    />
-                                </Animated.View>
-
-                                <View style={[switchStyles.textPos,
-                                                {
-                                                    width: this.props.switchWidth / 2 || this.state.sbWidth / 2,
-                                                    height: this.props.switchHeight - 6 || this.state.sbHeight - 6,
-                                                    left: 0
-                                                }]}
-                                >
-                                    <Text style={[ this.state.activeSwitch === 1 ? { color: this.props.activeFontColor || "#fff" } : { color: this.props.fontColor || "#b1b1b1" }  ]}>
-                                        { this.props.text1 || 'ON' }
-                                    </Text>
-                                </View>
-
-                                <View
-                                    style={[switchStyles.textPos,
-                                            {
-                                                width: this.props.switchWidth / 2 || this.state.sbWidth / 2,
-                                                height: this.props.switchHeight - 6 || this.state.sbHeight - 6,
-                                                right:0
-                                            }]}
-                                >
-                                    <Text style={[ this.state.activeSwitch === 2 ? { color: this.props.activeFontColor || "#fff" } : { color: this.props.fontColor || "#b1b1b1" }  ]}>
-                                    { this.props.text2 || 'OFF' }
-                                    </Text>
-                                </View>
-                            </View>
-
-                    </View>
-                    </TouchableOpacity>
-                    { this.props.children } 
-                </View>
-
-        );
-    }
-
+              <View
+                style={[
+                  switchStyles.textPos,
+                  {
+                    width: this.props.switchWidth / 2 || this.state.sbWidth / 2,
+                    height:
+                      this.props.switchHeight - 6 || this.state.sbHeight - 6,
+                    right: 0
+                  }
+                ]}
+              >
+                <Text
+                  style={[
+                    this.state.activeSwitch === 2
+                      ? { color: this.props.activeFontColor || '#000' }
+                      : { color: this.props.fontColor || '#817d84' }
+                  ], { fontWeight: 'bold' }}
+                >
+                  {this.props.text2 || 'OFF'}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </TouchableOpacity>
+        {this.props.children}
+      </View>
+    );
+  }
 }
 
 const switchStyles = StyleSheet.create({
-    textPos: {
-        position: 'absolute',
-        justifyContent: 'center',
-        alignItems: 'center'
+  textPos: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  rtl: {
+    flexDirection: 'row-reverse'
+  },
+  ltr: {
+    flexDirection: 'row'
+  },
+  wayBtnActive: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5
     },
-    rtl: {
-        flexDirection: 'row-reverse'
-    },
-    ltr: {
-        flexDirection: 'row'
-    },
-    wayBtnActive: {
-        borderWidth: 1,
-        marginTop: 2,
-        marginRight: 2,
-        marginLeft: 2
-    }
-
-  });
+    shadowOpacity: 0.4,
+    shadowRadius: 6,
+    elevation: 7
+  }
+});
