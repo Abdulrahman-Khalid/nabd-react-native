b43fe70f4271bf36a4cfc105079c232fb59bdffe
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2019-10-25 18:17:49 +0200

    Update Common Authentication Buttons Styling

--- src/app/welcome/CommonButtons.js
+++ src/app/welcome/CommonButtons.js
@@ -1,58 +1,93 @@
 import React, { Component } from 'react';
-import { Button } from '../../components';
 import { Actions } from 'react-native-router-flux';
 import { Colors } from '../../constants';
-import { Dimensions } from 'react-native';
-import { Block, Button as GaButton, theme } from 'galio-framework';
+import { Dimensions, View, TouchableOpacity, Text } from 'react-native';
 
 const { width, height } = Dimensions.get('screen');
 
 export default class CommonButtons extends Component {
   render() {
     return (
-      <Block center>
-        <Button
-          color="warning"
-          style={{ ...styles.button, ...styles.appColor }}
+      // <Block center>
+      //   <Button
+      //     color="warning"
+      //     style={{ ...styles.button, ...styles.appColor }}
+      //     onPress={() => Actions.signup()}
+      //     textStyle={styles.buttonTextNew}
+      //   >
+      //     I am new
+      //   </Button>
+      //   <Button
+      //     color="secondary"
+      //     style={{
+      //       ...styles.button,
+      //       ...{ borderColor: '#7a7a7a', borderWidth: 1 }
+      //     }}
+      //     textStyle={styles.buttonTextSignup}
+      //     onPress={() => Actions.signin()}
+      //   >
+      //     Sign in
+      //   </Button>
+      // </Block>
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity
+          style={styles.buttonContainer}
           onPress={() => Actions.signup()}
-          textStyle={styles.buttonTextNew}
         >
-          I am new
-        </Button>
-        <Button
-          color="secondary"
-          style={{
-            ...styles.button,
-            ...{ borderColor: '#7a7a7a', borderWidth: 1 }
-          }}
-          textStyle={styles.buttonTextSignup}
+          <View style={styles.button}>
+            <Text style={{ color: Colors.WHITE, fontFamily: 'Manjari-Bold' }}>
+              I'm new
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonContainer}
           onPress={() => Actions.signin()}
         >
-          Sign in
-        </Button>
-      </Block>
+          <View style={[styles.button, {backgroundColor: Colors.SECONDARY}]}>
+            <Text
+              style={{
+                color: Colors.SECONDARY_DARK,
+                fontFamily: 'Manjari-Bold'
+              }}
+            >
+              Sign in
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
 
 const styles = {
+  buttonsContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    margin: 10,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  buttonContainer: {
+    flex: 1,
+    padding: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
   button: {
-    marginBottom: theme.SIZES.BASE,
-    width: width - theme.SIZES.BASE * 2,
-    height: height / 15,
-    borderRadius: 5
-  },
-  appColor: {
+    height: 40,
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    flexDirection: 'row',
     backgroundColor: Colors.APP
-  },
-  buttonTextNew: {
-    color: Colors.WHITE,
-    fontSize: 20,
-    fontWeight: 'bold'
-  },
-  buttonTextSignup: {
-    color: '#7a7a7a',
-    fontSize: 20,
-    fontWeight: '700'
   }
 };
